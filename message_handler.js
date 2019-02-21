57a38d65b6795a4a8d50fea4a6d9bb4bba741d8d
robthemonster <robthemonsterlevin@gmail.com>
2019-02-20 21:38:48 -0500

    Major changes to the way data is served. No longer calling mtg api from my api (doing that client side)

--- message_handler.js
+++ message_handler.js
@@ -27,7 +27,6 @@ aws.config.update({region: 'us-east-2', accessKeyId: process.env.AWS_API_ID, sec
 let db = new aws.DynamoDB({apiVersion: '2019-02-16'});
 const LIKED_TABLE = "cards_liked";
 const BLOCKED_TABLE = "cards_blocked";
-let mtg = require('mtgsdk');
 
 function randomInt(min, max) {
     return min + Math.floor(Math.random() * (max - min));
@@ -40,6 +39,48 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
+function queryAllForUserParams(tablename, userid) {
+    return {
+        TableName: tablename,
+        KeyConditionExpression: "userid = :userid",
+        ExpressionAttributeValues: {":userid": {'S': userid}}
+    };
+}
+
+app.post('/getLiked', (req, res, next) => {
+    let userid = req.body.userid;
+    let likedUuids = [];
+    db.query(queryAllForUserParams(LIKED_TABLE, userid)).promise().then(result => {
+        result.Items.forEach(item => {
+            let uuid = item.uuid.S;
+            let name = cards[cardKeys[uuidToIndex[uuid]]].name;
+            likedUuids.push({uuid: uuid, name: name});
+        });
+        likedUuids.sort((a,b) => {return a.name < b.name ? -1 : 1});
+        res.json(likedUuids);
+    });
+});
+app.post('/getBlocked', (req, res, next) => {
+    let userid = req.body.userid;
+    let blockedUuids = [];
+    db.query(queryAllForUserParams(BLOCKED_TABLE, userid)).promise().then(result => {
+        result.Items.forEach(item => {
+            let uuid = item.uuid.S;
+            let name = cards[cardKeys[uuid]].name;
+            blockedUuids.push({uuid: uuid, name: name});
+        });
+        res.json(blockedUuids);
+    });
+});
+
+async function putItemInTable(tablename, item) {
+    return db.putItem({TableName: tablename, Item: item}).promise();
+}
+
+async function removeItemFromTable(tablename, key) {
+    return db.deleteItem({TableName: tablename, Key: key}).promise();
+}
 
 app.post('/addCardToLiked', (req, res, next) => {
     let userid = req.body.userid;
@@ -59,7 +100,6 @@ app.post('/addCardToBlocked', (req, res, next) => {
     let uuid = req.body.uuid;
     putItemInTable(BLOCKED_TABLE, {'userid': {'S': userid}, 'uuid': {'S': uuid}})
         .then(result => {
-            console.log("successfully added to blocked");
             res.json(result);
         })
         .catch(error => {
@@ -67,39 +107,74 @@ app.post('/addCardToBlocked', (req, res, next) => {
         });
 });
 
-async function putItemInTable(tablename, item) {
-    return db.putItem({TableName: tablename, Item: item}).promise();
-}
+app.post('/removeCardFromLiked', (req, res, next) => {
+    let userid = req.body.userid;
+    let uuid = req.body.uuid;
+    removeItemFromTable(LIKED_TABLE, {'userid': {'S': userid}, 'uuid': {'S': uuid}})
+        .then(result => {
+            res.json(result);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+});
+
+app.post('/removeCardFromBlocked', (req, res, next) => {
+    let userid = req.body.userid;
+    let uuid = req.body.uuid;
+    removeItemFromTable(BLOCKED_TABLE, {'userid': {'S': userid}, 'uuid': {'S': uuid}})
+        .then(result => {
+            res.json(result);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+});
+
+app.post('/searchForCard', (req, res, next) => {
+    let searchString = req.body.searchString.toLowerCase();
+    let userid = req.body.userid;
+    let likedParams = queryAllForUserParams(LIKED_TABLE, userid);
+    let blockedParams = queryAllForUserParams(BLOCKED_TABLE, userid);
+    let taken = new Set();
+    let liked_promise = db.query(likedParams).promise();
+    let blocked_promise = db.query(blockedParams).promise();
+    Promise.all([liked_promise, blocked_promise]).then(([res1, res2]) => {
+        res1.Items.forEach(item => {
+            taken.add(item.uuid);
+        });
+        res2.Items.forEach(item => {
+            taken.add(item.uuid);
+        });
+    });
+    let results = [];
+    for (let key in cardKeys) {
+        let card = cards[cardKeys[key]];
+        if (!taken.has(card.uuid) && card.name.toLowerCase().startsWith(searchString)) {
+            results.push(card.uuid);
+        }
+    }
+    res.json(results);
+});
+
 
 app.post("/randomCard", (req, res, next) => {
     let userid = req.body.userid;
     console.log("userid " + userid);
     let uuid = cards[cardKeys[randomInt(0, cardKeys.length - 1)]].uuid;
-    let taken = new Set();
-    let likedParams = getParams(LIKED_TABLE);
-    let blockedParams = getParams(BLOCKED_TABLE);
+    let likedParams = queryAllForUserParams(LIKED_TABLE, userid);
+    let blockedParams = queryAllForUserParams(BLOCKED_TABLE, userid);
+    let liked_promise = db.query(likedParams).promise();
+    let blocked_promise = db.query(blockedParams).promise();
 
-    function getParams(tablename) {
-        return {
-            TableName: tablename,
-            KeyConditionExpression: "userid = :userid",
-            ExpressionAttributeValues: {":userid": {'S': userid}}
-        };
-    }
-
-    function addUuidToTaken(error, results) {
-        if (error) {
-            console.log(error);
-        }
-        for (let item in results.Items) {
-            taken.add(results.Items[item].uuid);
-        }
-    }
-
-    let liked_promise = db.query(likedParams, addUuidToTaken).promise();
-    let blocked_promise = db.query(blockedParams, addUuidToTaken).promise();
-
-    Promise.all([liked_promise, blocked_promise]).then((res1, res2) => {
+    Promise.all([liked_promise, blocked_promise]).then(([res1, res2]) => {
+        let taken = new Set();
+        res1.Items.forEach(item => {
+            taken.add(item.uuid);
+        });
+        res2.Items.forEach(item => {
+            taken.add(item.uuid);
+        });
         if (taken.has(uuid)) {
             if (taken.size >= cardKeys.length / 2) {
                 let rand = [];
@@ -116,7 +191,7 @@ app.post("/randomCard", (req, res, next) => {
                 }
             }
         }
-        mtg.card.find(uuid).then(result => res.json(result)).catch(printError);
+        res.json(uuid);
     });
 });
 
