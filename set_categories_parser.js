640ad5ec89921851c781ea7b30420e7c86f63edc
robthemonster <robthemonsterlevin@gmail.com>
2019-03-21 02:08:19 -0400

    set icons parser

--- set_categories_parser.js
+++ set_categories_parser.js
@@ -1,13 +1,13 @@
 const scryfall = require('scryfall-sdk');
 const fs = require('fs');
 let set_groups = {
-    core:[],
-    commander:[],
-    duel_deck:[]
+    core: [],
+    commander: [],
+    duel_deck: []
 };
+let set_icons = {};
 const axios = require('axios');
 axios.post("https://api.tapandresolve.tk/getSetCodes", {Headers: {'Content-type': 'application/json'}}).then((res) => {
-
     let tap_has = new Set(res.data.map(set => set.code));
     scryfall.Sets.all().then(results => {
         results.forEach(set => {
@@ -15,9 +15,11 @@ axios.post("https://api.tapandresolve.tk/getSetCodes", {Headers: {'Content-type'
                 set_groups[set.set_type] = [];
             }
             if (tap_has.has(set.code)) {
+                set_icons[set.code] = set.icon_svg_uri;
                 set_groups[set.set_type].push(set.code);
             }
         });
+        fs.writeFileSync('./set_icons.json', JSON.stringify(set_icons));
         fs.writeFileSync("./set_groups.json", JSON.stringify(set_groups));
     });
 });
